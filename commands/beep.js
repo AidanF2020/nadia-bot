ddfb24f20dc10e826ea7b62a678bf9148b5fbfb4
Aidan Fleming <aidanf2020@gmail.com>
2022-08-11 05:39:26 +0000

    Test of gitpod and intellij compatability 2

--- commands/beep.js
+++ commands/beep.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('beep')
-		.setDescription('Beep!'),
+		.setDescription('Beeps!'),
 	async execute(interaction) {
 		return interaction.reply('Boops!');
 	},
